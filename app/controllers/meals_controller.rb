5780049c075554522a857b9fc3ab721584b429ea
mimimi32k <comame.mizuki@gmail.com>
2019-05-16 11:51:12 +0000

    devise

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,10 +1,20 @@
 class MealsController < ApplicationController
   
   def index
-    @meals = Meal.all
+    @meals = Meal.all.order("created_at DESC").page(params[:page]).per(5)
   end
   
   def new
   end
   
+  def create
+    Meal.create(meal_params)
+   
+  end
+  
+   private
+  def meal_params
+    params.permit(:name, :image, :text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'meals' => 'meals#index' 
-  get  'meals/new'  =>  'meals#new'       #ツイート投稿画面# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root  'meals#index'      #ルートパスの指定
+  get   'meals'      => 'meals#index'       #ツイート一覧画面  
+  get   'meals/new'  =>  'meals#new'        #ツイート投稿画面
+  post  'meals'      =>  'meals#create'     #ツイート投稿機能#ツイート投稿画面# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
