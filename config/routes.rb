07ac126646174f3c21b773f441906eacdba58048
mimimi32k <comame.mizuki@gmail.com>
2019-05-22 22:42:21 +0000

    resources nest

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'meals#index'      #ルートパスの指定
-  get   'meals'      => 'meals#index'       #ツイート一覧画面  
-  get   'meals/new'  =>  'meals#new'        #ツイート投稿画面
-  post  'meals'      =>  'meals#create'     #ツイート投稿機能#ツイート投稿画面
-  get   'users/:id'   =>  'users#show'    #Mypageへのルーティング　　　　　　　　
-  delete  'meals/:id'  => 'meals#destroy'
-  get   'meals/:id/edit'  => 'meals#edit'
-  patch   'meals/:id'  => 'meals#update' 
-  get 'meals/:id' => 'meals#show'       #ツイート詳細画面  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :meals                     #meals_controllerに対してのresourcesメソッド
+  resources :meals do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
 end
